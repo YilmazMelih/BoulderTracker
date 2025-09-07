d3a3ea61649699c6de1debaae1e7e893e7ce8d90
Melih Yilmaz <melihyilmaz999@gmail.com>
2025-09-07 12:22:05 -0400

    Added vercel analytics

--- frontend/boulder-tracker/src/main.jsx
+++ frontend/boulder-tracker/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "@/components/ui/provider";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/next";
 import "./index.css";
 import App from "./App.jsx";
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")).render(
         <Provider>
             <App />
             <Toaster />
+            <Analytics />
         </Provider>
     </StrictMode>
 );
